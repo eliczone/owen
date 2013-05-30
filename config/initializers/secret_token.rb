aa563106414958a3b4c92f1f516468f604cab32d
eliczone <eliczone@gmail.com>
2013-05-30 12:09:46 +0800

    owen commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Owen::Application.config.secret_token = '5a95c4b91d517029ef72b0297783b4c5a22a38e1dd3ba7c3f4ddb2d1b42b25e02bb01b2fddf908044a3f7f884fb1ec749cebc666e5b0675e87807c1bb9b7105b'
